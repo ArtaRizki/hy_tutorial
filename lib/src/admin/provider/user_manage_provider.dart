16be1fb381a415243ef035ef2f1644c1a134bd6b
ArtaRizki <yuliarta_rizki_nusantoko@yahoo.co.id>
2024-07-12 10:57:28 +0700

    Manage User & Detail User

--- lib/src/admin/provider/user_manage_provider.dart
+++ lib/src/admin/provider/user_manage_provider.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:powers/powers.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../../../common/component/custom_alert.dart';
 import '../../../main.dart';
 import '../../../utils/utils.dart';
 import '../../../common/base/base_controller.dart';
@@ -11,9 +14,326 @@ import '../../../common/helper/constant.dart';
 import '../../../common/component/custom_dropdown.dart';
 import '../../../common/component/custom_textfield.dart';
 import '../model/user_detail_model.dart';
+import '../model/user_list_model.dart';
 
 class UserManageProvider extends BaseController with ChangeNotifier {
   GlobalKey<FormState> userAddKey = GlobalKey<FormState>();
+
+  PagingController<int, UserListModelData> _pagingController =
+      PagingController(firstPageKey: 1);
+
+  PagingController<int, UserListModelData> get pagingController =>
+      this._pagingController;
+
+  set pagingController(PagingController<int, UserListModelData> value) {
+    this._pagingController = value;
+  }
+
+  PagingController<int, UserListModelData> _pagingController2 =
+      PagingController(firstPageKey: 1);
+
+  PagingController<int, UserListModelData> get pagingController2 =>
+      this._pagingController2;
+
+  set pagingController2(PagingController<int, UserListModelData> value) {
+    this._pagingController2 = value;
+  }
+
+  Duration duration = const Duration(seconds: 2);
+  Timer? _searchOnStoppedTyping;
+  Timer? get searchOnStoppedTyping => this._searchOnStoppedTyping;
+
+  set searchOnStoppedTyping(Timer? value) {
+    this._searchOnStoppedTyping = value;
+    notifyListeners();
+  }
+
+  bool _isFetching = false;
+  bool get isFetching => this._isFetching;
+
+  set isFetching(bool value) {
+    this._isFetching = value;
+  }
+
+  TextEditingController userSearchC = TextEditingController();
+  Duration duration2 = const Duration(seconds: 2);
+  Timer? _searchOnStoppedTyping2;
+  Timer? get searchOnStoppedTyping2 => this._searchOnStoppedTyping2;
+
+  set searchOnStoppedTyping2(Timer? value) {
+    this._searchOnStoppedTyping2 = value;
+    notifyListeners();
+  }
+
+  bool _isFetching2 = false;
+  bool get isFetching2 => this._isFetching2;
+
+  set isFetching2(bool value) {
+    this._isFetching2 = value;
+  }
+
+  TextEditingController userSearchC2 = TextEditingController();
+
+  bool _ascending = false;
+  bool get ascending => this._ascending;
+  set ascending(bool value) => this._ascending = value;
+  bool _descending = false;
+  bool get descending => this._descending;
+  set descending(bool value) => this._descending = value;
+
+  bool _createdAt = false;
+  bool get createdAt => this._createdAt;
+  set createdAt(bool value) => this._createdAt = value;
+  bool _username = false;
+  bool get username => this._username;
+  set username(bool value) => this._username = value;
+
+  int pageSize = 0;
+
+  get getPageSize => this.pageSize;
+
+  set setPageSize(pageSize) {
+    this.pageSize = pageSize;
+    notifyListeners();
+  }
+
+  bool _ascending2 = false;
+  bool get ascending2 => this._ascending2;
+  set ascending2(bool value) => this._ascending2 = value;
+  bool _descending2 = false;
+  bool get descending2 => this._descending2;
+  set descending2(bool value) => this._descending2 = value;
+
+  bool _createdAt2 = false;
+  bool get createdAt2 => this._createdAt2;
+  set createdAt2(bool value) => this._createdAt2 = value;
+  bool _username2 = false;
+  bool get username2 => this._username2;
+  set username2(bool value) => this._username2 = value;
+
+  int pageSize2 = 0;
+
+  get getPageSize2 => this.pageSize2;
+
+  set setPageSize2(pageSize) {
+    this.pageSize2 = pageSize;
+    notifyListeners();
+  }
+
+  String? next;
+  String? next2;
+
+  Future<void> getUserList() async {
+    pagingController = PagingController(firstPageKey: 1)
+      ..addPageRequestListener((pageKey) async {
+        log("GET USER");
+        await fetchUser(page: pageKey).onError((error, stackTrace) {
+          if (error.toString().contains('expired token')) {
+            log("ERROR EXPIRED TOKEN");
+            next = null;
+            pagingController.refresh();
+          } else {
+            BuildContext? context =
+                NavigationService.navigatorKey.currentContext;
+            if (context != null)
+              CustomAlert.showSnackBar(
+                  context, 'Gagal Mendapatkan Data User', true);
+          }
+        });
+      });
+  }
+
+  Future<void> getUserList2() async {
+    pagingController2 = PagingController(firstPageKey: 1)
+      ..addPageRequestListener((pageKey) async {
+        log("GET USER 2");
+        await fetchUser2(page: pageKey).onError((error, stackTrace) {
+          if (error.toString().contains('expired token')) {
+            log("ERROR EXPIRED TOKEN");
+            next2 = null;
+            pagingController2.refresh();
+          } else {
+            BuildContext? context =
+                NavigationService.navigatorKey.currentContext;
+            if (context != null)
+              CustomAlert.showSnackBar(
+                  context, 'Gagal Mendapatkan Data User', true);
+          }
+        });
+      });
+  }
+
+  Future<void> fetchUser({
+    bool withLoading = false,
+    required int page,
+    String keyword = "",
+  }) async {
+    try {
+      if (!isFetching) {
+        isFetching = true;
+        if (withLoading) loading(true);
+        String url = Constant.BASE_API_FULL + '/admin/users';
+        Map<String, String> param = {};
+        if (userSearchC.text.isNotEmpty)
+          param.addAll({'Search': userSearchC.text});
+        if (ascending) {
+          param.remove('SortOrder');
+          param.addAll({'SortOrder': 'ASC'});
+        }
+        if (descending) {
+          param.remove('SortOrder');
+          param.addAll({'SortOrder': 'DESC'});
+        }
+        if (username) {
+          param.remove('SortBy');
+          param.addAll({'SortBy': 'Username'});
+        }
+        if (createdAt) {
+          param.remove('SortBy');
+          param.addAll({'SortBy': 'CreatedAt'});
+        }
+        if (next != null && next != '') param.addAll({'Next': next ?? ''});
+        log("PANGGIL");
+        if (_pagingController.itemList?.length != 0) {
+          await Future.delayed(Duration(seconds: 1));
+        }
+        final response = await get(
+          url,
+          body: param,
+        );
+
+        if (response.statusCode == 201 || response.statusCode == 200) {
+          final model = UserListModel.fromJson(jsonDecode(response.body));
+          final items = model.Data ?? [];
+          List<UserListModelData?> newItems;
+          newItems =
+              items.where((element) => element?.Status == 'active').toList();
+
+          // userModel = model;
+          // notifyListeners();
+
+          final previouslyFetchedWordCount =
+              _pagingController.itemList?.length ?? 0;
+          pageSize = 10;
+          log("ITEMS LENGTH : ${newItems.length}");
+          final isLastPage = newItems.length < pageSize;
+
+          if (isLastPage) {
+            next = null;
+            pagingController
+                .appendLastPage(newItems as List<UserListModelData>);
+          } else {
+            final nextPageKey = page += 1;
+            if (model.Meta?.Next != null && model.Meta?.Next != '')
+              next = model.Meta?.Next ?? '';
+            pagingController.appendPage(
+                newItems as List<UserListModelData>, nextPageKey);
+          }
+
+          // notifyListeners();
+          if (withLoading) loading(false);
+          isFetching = false;
+        } else {
+          log("MASUK ELSE");
+          loading(false);
+          isFetching = false;
+          final message = jsonDecode(response.body)["Message"];
+          throw Exception(message);
+        }
+      }
+    } catch (e) {
+      log("MASUK ELSE CATCH $e");
+      loading(false);
+      isFetching = false;
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<void> fetchUser2({
+    bool withLoading = false,
+    required int page,
+    String keyword = "",
+  }) async {
+    try {
+      if (!isFetching2) {
+        isFetching2 = true;
+        if (withLoading) loading(true);
+        String url = Constant.BASE_API_FULL + '/admin/users';
+        Map<String, String> param = {};
+        if (userSearchC2.text.isNotEmpty)
+          param.addAll({'Search': userSearchC2.text});
+        if (ascending2) {
+          param.remove('SortOrder');
+          param.addAll({'SortOrder': 'ASC'});
+        }
+        if (descending2) {
+          param.remove('SortOrder');
+          param.addAll({'SortOrder': 'DESC'});
+        }
+        if (username2) {
+          param.remove('SortBy');
+          param.addAll({'SortBy': 'Username'});
+        }
+        if (createdAt2) {
+          param.remove('SortBy');
+          param.addAll({'SortBy': 'CreatedAt'});
+        }
+        if (next2 != null && next2 != '') param.addAll({'Next': next2 ?? ''});
+        log("PANGGIL");
+        if (_pagingController2.itemList?.length != 0) {
+          await Future.delayed(Duration(seconds: 1));
+        }
+        final response = await get(
+          url,
+          body: param,
+        );
+
+        if (response.statusCode == 201 || response.statusCode == 200) {
+          final model = UserListModel.fromJson(jsonDecode(response.body));
+          final items = model.Data ?? [];
+          List<UserListModelData?> newItems;
+          newItems =
+              items.where((element) => element?.Status != 'active').toList();
+
+          // userModel = model;
+          // notifyListeners();
+
+          final previouslyFetchedWordCount =
+              _pagingController2.itemList?.length ?? 0;
+          pageSize2 = 10;
+          log("ITEMS LENGTH : ${newItems.length}");
+          final isLastPage = newItems.length < pageSize2;
+
+          if (isLastPage) {
+            next2 = null;
+            pagingController2
+                .appendLastPage(newItems as List<UserListModelData>);
+          } else {
+            final nextPageKey = page += 1;
+            if (model.Meta?.Next != null && model.Meta?.Next != '')
+              next2 = model.Meta?.Next ?? '';
+            pagingController2.appendPage(
+                newItems as List<UserListModelData>, nextPageKey);
+          }
+
+          // notifyListeners();
+          if (withLoading) loading(false);
+          isFetching2 = false;
+        } else {
+          log("MASUK ELSE");
+          loading(false);
+          isFetching2 = false;
+          final message = jsonDecode(response.body)["Message"];
+          throw Exception(message);
+        }
+      }
+    } catch (e) {
+      log("MASUK ELSE CATCH $e");
+      loading(false);
+      isFetching2 = false;
+      throw Exception(e.toString());
+    }
+  }
 
   TextEditingController nameC = TextEditingController();
   TextEditingController nipC = TextEditingController();
@@ -42,16 +362,17 @@ class UserManageProvider extends BaseController with ChangeNotifier {
 
   UserDetailModel _userDetailModel = UserDetailModel();
   UserDetailModel get userDetailModel => this._userDetailModel;
-  set userDetailModel (UserDetailModel value) => this._userDetailModel = value;
+  set userDetailModel(UserDetailModel value) => this._userDetailModel = value;
 
-  Future<UserDetailModel> fetchUserDetail({required String id}) async {
+  Future<void> fetchUserDetail({required String id}) async {
     loading(true);
-    final response = await get(Constant.BASE_API_FULL + 'admin/users/$id');
+    final response = await get(Constant.BASE_API_FULL + '/admin/users/$id');
 
     if (response.statusCode == 201 || response.statusCode == 200) {
       final model = UserDetailModel.fromJson(jsonDecode(response.body));
+      userDetailModel = model;
+      notifyListeners();
       loading(false);
-      return model;
     } else {
       final message = jsonDecode(response.body)["Message"];
       loading(false);
@@ -120,5 +441,4 @@ class UserManageProvider extends BaseController with ChangeNotifier {
       Constant.xSizedBox16,
     ];
   }
-  
 }
--- lib/src/admin/view/user_detail_view.dart
+++ lib/src/admin/view/user_detail_view.dart
@@ -2,6 +2,8 @@ import 'dart:developer';
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:hy_tutorial/common/component/custom_navigator.dart';
+import 'package:hy_tutorial/src/admin/view/user_add_view.dart';
 import '../../../common/component/custom_appbar.dart';
 import '../../../common/component/custom_button.dart';
 import '../../../common/component/custom_container.dart';
@@ -27,10 +29,27 @@ class _UserDetailViewState extends State<UserDetailView>
 
   @override
   Widget build(BuildContext context) {
-    final user = context.watch<UserManageProvider>().userDetailModel.Data;
+    final userP = context.watch<UserManageProvider>().userDetailModel.Data;
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: CustomAppBar.appBar(context, "Detail User"),
+      appBar: CustomAppBar.appBar(context, "Detail User", action: [
+        IconButton(
+          onPressed: () {
+            CusNav.nPush(context, UserAddView());
+          },
+          icon: Icon(
+            Icons.edit,
+            color: Constant.primaryColor,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.delete,
+            color: Constant.redColor,
+          ),
+        ),
+      ]),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
         child: Column(
@@ -59,7 +78,7 @@ class _UserDetailViewState extends State<UserDetailView>
                         Expanded(
                           flex: 5,
                           child: Text(
-                            '${user?.Name ?? "Jhon Doe"}',
+                            '${userP?.Name ?? "Jhon Doe"}',
                             style: TextStyle(fontWeight: FontWeight.w500),
                           ),
                         ),
@@ -82,7 +101,7 @@ class _UserDetailViewState extends State<UserDetailView>
                         Expanded(
                           flex: 5,
                           child: Text(
-                            '${user?.Division ?? "Engineer"}',
+                            '${userP?.Division ?? "Engineer"}',
                             style: TextStyle(fontWeight: FontWeight.w500),
                           ),
                         ),
@@ -105,7 +124,7 @@ class _UserDetailViewState extends State<UserDetailView>
                         Expanded(
                           flex: 5,
                           child: Text(
-                            '${user?.Role ?? "User"}',
+                            '${userP?.Role ?? "User"}',
                             style: TextStyle(fontWeight: FontWeight.w500),
                           ),
                         ),
@@ -128,7 +147,7 @@ class _UserDetailViewState extends State<UserDetailView>
                         Expanded(
                           flex: 5,
                           child: Text(
-                            '${user?.Username ?? "Jhon Doe"}',
+                            '${userP?.Username ?? "Jhon Doe"}',
                             style: TextStyle(fontWeight: FontWeight.w500),
                           ),
                         ),
@@ -151,7 +170,7 @@ class _UserDetailViewState extends State<UserDetailView>
                         Expanded(
                           flex: 5,
                           child: Text(
-                            '${user?.Status ?? "Jhon 123"}',
+                            '${userP?.Status ?? "Jhon 123"}',
                             style: TextStyle(fontWeight: FontWeight.w500),
                           ),
                         ),
@@ -161,28 +180,28 @@ class _UserDetailViewState extends State<UserDetailView>
                 ],
               ),
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 0, vertical: 10),
-              child: CustomButton.mainButton(
-                'Submit',
-                () {
-                  final dataP = context.read<UserManageProvider>();
-                  FocusManager.instance.primaryFocus?.unfocus();
-                  String? msg;
-                  //if (dataP.nameC.text.isEmpty) msg = 'Harap Isi Nama Lengkap';
-                  //if (dataP.nipC.text.isEmpty) msg = 'Harap Isi NIP';
-                  //if (dataP.roleC.text.isEmpty) msg = 'Harap Pilih Role';
-                  //if (dataP.usernameC.text.isEmpty) msg = 'Harap Isi Username';
-                  //if (dataP.passwordC.text.isEmpty) msg = 'Harap Isi Password';
-                  if (msg != null) {
-                    Utils.showFailed(msg: msg);
-                    return;
-                  } else {
-                    // Navigator
-                  }
-                },
-              ),
-            ),
+            // Padding(
+            //   padding: EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+            //   child: CustomButton.mainButton(
+            //     'Submit',
+            //     () {
+            //       final dataP = context.read<UserManageProvider>();
+            //       FocusManager.instance.primaryFocus?.unfocus();
+            //       String? msg;
+            //       //if (dataP.nameC.text.isEmpty) msg = 'Harap Isi Nama Lengkap';
+            //       //if (dataP.nipC.text.isEmpty) msg = 'Harap Isi NIP';
+            //       //if (dataP.roleC.text.isEmpty) msg = 'Harap Pilih Role';
+            //       //if (dataP.usernameC.text.isEmpty) msg = 'Harap Isi Username';
+            //       //if (dataP.passwordC.text.isEmpty) msg = 'Harap Isi Password';
+            //       if (msg != null) {
+            //         Utils.showFailed(msg: msg);
+            //         return;
+            //       } else {
+            //         // Navigator
+            //       }
+            //     },
+            //   ),
+            // ),
           ],
         ),
       ),
